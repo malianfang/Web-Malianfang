7d97ff11275d52453f3fff749068a66076d10790
用户名 <>
2025-04-23 10:17:07 +0800

    第八周

---
+++ my-next-app/src/app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>自定义内容</h1>
+      <h2>
+        第一天
+      </h2>
+    </div>
+  );
+} 
