1c94ce7536dc8ae98f430a800164f50232657130
用户名 <>
2025-05-07 10:19:54 +0800

    第十周

---
+++ tailwind/src/app/components/Navbar.js
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <div>
+      <h1>这是来自函数的内容</h1>
+    </div>
+  );
+}
---
+++ tailwind/src/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-rose-700">Home</h1>
+      <p className="text-stone-800">Welcome to my homepage! </p>
+    </div>
+  );
+}
